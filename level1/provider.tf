65586385226bdb1e34338d5cb26a7e2bb65c4e59
Helen <helenofalodun@gmail.com>
2022-10-03 16:13:52 -0400

    Create VPC & Components

---
+++ level1/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "tf-remote-state-8805-virginia"
+    key            = "level1.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-remote"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
